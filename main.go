91f4261a4b8c47496b3ab60e20f0b9a21515eb90
dimm <witty.sock3113@fastmail.com>
2022-09-02 06:50:30 +0200

    v1

--- main.go
+++ main.go
@@ -8,5 +8,23 @@ func main() {
 }
 
 func maxProfit(prices []int) int {
+	type stock struct {
+		day   int
+		price int
+	}
+	var min stock
+	var max stock
 
+	for i, p := range prices {
+		if p < min.price {
+			min.day = i
+			min.price = p
+		}
+		if p > max.price {
+			max.day = i
+			max.price = p
+		}
+
+	}
+	return 0
 }

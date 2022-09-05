640b4e27e9c17468789189daa08ed6a8c5e46ba9
dimm <witty.sock3113@fastmail.com>
2022-09-05 07:11:04 +0200

    try to remove odd tail

--- main.go
+++ main.go
@@ -4,30 +4,30 @@ import "fmt"
 
 func main() {
 	prices := []int{7, 1, 5, 3, 6, 4}
-	prices = []int{7, 6, 4, 3, 1}
+	prices = []int{2, 4, 1}
 	fmt.Println(maxProfit(prices))
 }
 
 func maxProfit(prices []int) int {
-	type stock struct {
-		day   int
-		price int
-	}
-	var min stock
-	var max stock
+	type stock int
+
+	var min, max, profit int
+	var day int
 
 	for i, p := range prices {
-		if p < min.price || min.price == 0 {
-			min.day = i
-			min.price = p
+		if (p < min && i != len(prices)) || min == 0 {
+			day = i
+			min = p
 		}
-		if p > max.price && i > min.day {
-			max.day = i
-			max.price = p
+		if p > max && i > day {
+			max = p
+		}
+
+		if profit < max-min {
+			profit = max - min
 		}
 
 	}
-	profit := max.price - min.price
 	if profit < 0 {
 		profit = 0
 	}
